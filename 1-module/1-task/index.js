a2c149bb6b5bcdff01a99a12de3bfe1f95994361
Константин Разинков <konstantinrazinkov@Air-Konstantin.lan>
2022-07-14 14:52:41 +0300

    факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
   // ваш код...
+  for (let SomeNum = n - 1; SomeNum > 0; SomeNum--) {
+    n = n * SomeNum;
+  }
+  if (n == 0){
+    return(1);
+  } else {
+    return(n);
+  }
 }
