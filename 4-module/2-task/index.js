2d1f5d9f4fe8ac23ce8fe80c42644c938eb467ab
Konstantin Razinkov <1604177123@mail.ru>
2022-07-31 17:06:54 +0300

    4 модуль

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
   // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    row.cells[i].style.backgroundColor = 'red';
+  }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,19 @@
 function highlight(table) {
   // ваш код...
+  for (let row of table.rows) {
+
+    if (row.cells[3].dataset.available === 'true') {
+      row.classList.toggle('available', true);
+    } else if (row.cells[3].dataset.available === 'false') {
+      row.classList.toggle('unavailable', true);
+    } else if (!row.cells[3].hasAttribute('data-available')) {
+      row.hidden = true;
+    }
+
+    let genClass = row.cells[2].innerText === 'm' ? 'male' : 'female';
+    row.classList.add(genClass);
+
+    if (parseInt(row.cells[1].innerText) < 18)
+      row.style.textDecoration = 'line-through';
+  }
 }
