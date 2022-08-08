96d6f23f9bf80372b9bd4a87f548b738c9d92596
Konstantin Razinkov <1604177123@mail.ru>
2022-08-08 15:53:39 +0300

    5 модуль

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
   // ваш код...
+  const rightBtn = document.querySelector('.carousel__arrow_right');
+  const leftBtn = document.querySelector('.carousel__arrow_left');
+  const inner = document.querySelector('.carousel__inner');
+  const maxSlide = 4;
+  let count = 0;
+  hideBtn();
+  
+  rightBtn.addEventListener('click', () => {
+    count--;
+    inner.style.transform = 'translateX(' + inner.offsetWidth * count +'px)';
+    hideBtn();
+  })
+
+  leftBtn.addEventListener('click', () => {
+    count++;
+    inner.style.transform = 'translateX(' + inner.offsetWidth * count +'px)'
+    hideBtn();
+  })
+
+  function hideBtn () {
+    if (count <= (maxSlide -1)) {
+      leftBtn.style.display = '';
+      rightBtn.style.display = 'none';
+    }
+    if (count >= 0) {
+      rightBtn.style.display = '';
+      leftBtn.style.display = 'none';
+    }
+    if ((count < 0) && (count > -(maxSlide - 1))) {
+      rightBtn.style.display = '';
+      leftBtn.style.display = '';
+    }
+  }
 }
