88a6f7fd5f1ee3afa53bb7f34640096e6d4e7559
Konstantin <1604177123@mail.ru>
2025-06-28 22:41:41 +0300

    promise module

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -1,4 +1,7 @@
-export default function promiseClick(button) {
-  // ваш код...
-  
-}
+  export default function promiseClick(button) {
+    return new Promise((resolve) => {
+      button.addEventListener('click', (event) => {
+        resolve(event);
+      })
+    }, {once: true})
+ }
